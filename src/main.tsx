44dbe15596c2fbfd3387f5d0647f40f4015d5337
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-06-20 15:08:29 +0200

    remove React strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "urql";
+import { graphqlClient } from "./graphql-client.ts";
 import App from "./components/App/App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
+  <Provider value={graphqlClient}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
