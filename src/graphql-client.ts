120e4bfd36c4daa96ab23c9e1874cea3f612dd46
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-06-20 15:07:43 +0200

    update file layout

--- src/graphql-client.ts
+++ src/graphql-client.ts
@@ -1,26 +1,26 @@
-import { Client, fetchExchange, subscriptionExchange } from "urql";
-import { createClient as createWSClient } from "graphql-ws";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { Client, fetchExchange, subscriptionExchange } from 'urql';
+import { createClient as createWSClient } from 'graphql-ws';
+import { cacheExchange } from '@urql/exchange-graphcache';
 
 const wsClient = createWSClient({
-  url: "wss://impostor-server.fly.dev/graphql",
+  url: 'wss://impostor-server.fly.dev/graphql'
 });
 
 export const graphqlClient = new Client({
-  url: "https://impostor-server.fly.dev/graphql",
+  url: 'https://impostor-server.fly.dev/graphql',
   exchanges: [
     cacheExchange({}),
     fetchExchange,
     subscriptionExchange({
       forwardSubscription(request) {
-        const input = { ...request, query: request.query || "" };
+        const input = { ...request, query: request.query || '' };
         return {
           subscribe(sink) {
             const unsubscribe = wsClient.subscribe(input, sink);
             return { unsubscribe };
-          },
+          }
         };
-      },
-    }),
-  ],
+      }
+    })
+  ]
 });
