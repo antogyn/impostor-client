e5ae69305a28d6c6f1fc95e9afa7deb3fc956aed
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-10 13:07:51 +0200

    implement QR code option with basic style and layout

--- src/components/Room/RoomOptions/RoomOptions.tsx
+++ src/components/Room/RoomOptions/RoomOptions.tsx
@@ -1,5 +1,5 @@
 import { graphql } from "gql.tada";
-import { type FormEvent, useContext } from "react";
+import { type FormEvent, useContext, useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { ToastContainer, toast } from "react-toastify";
 import { useMutation } from "urql";
@@ -9,6 +9,17 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { Button } from "../../ui/button";
 import { DoorOpen, Users } from "lucide-react";
+
+import QRCode from "react-qr-code";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface RoomProps {
   id: number;
@@ -29,6 +40,7 @@ const LeaveRoomMutation = graphql(`
 export const RoomOptions = ({ id }: RoomProps) => {
   const [name] = useContext(NameContext);
   const [_leaveRoomResult, leaveRoom] = useMutation(LeaveRoomMutation);
+  const [qrIsVisible, setQrIsVisible] = useState<boolean>(false);
 
   const roomUrl = location.href;
   console.log("Current url is:", roomUrl);
@@ -56,11 +68,40 @@ export const RoomOptions = ({ id }: RoomProps) => {
           Leave Room
           <DoorOpen className="ml-3 max-sm:hidden" />
         </Button>
-        <CopyToClipboard text={roomUrl} onCopy={onCopyUrl}>
-          <Button type="button" size="lg" variant="cta">
-            Invite Friends <Users className="ml-3 max-sm:hidden" />
-          </Button>
-        </CopyToClipboard>
+        <Dialog>
+          <DialogTrigger>
+            <Button type="button" size="lg" variant="cta">
+              Invite friends
+              <Users className="ml-3 max-sm:hidden" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent onCloseAutoFocus={() => setQrIsVisible(false)}>
+            <DialogHeader>
+              <DialogTitle>Select Option</DialogTitle>
+              <DialogDescription>
+                <CopyToClipboard text={roomUrl} onCopy={onCopyUrl}>
+                  <Button type="button" size="lg" variant="cta">
+                    Copy Link <Users className="ml-3 max-sm:hidden" />
+                  </Button>
+                </CopyToClipboard>
+                <p>OR</p>
+                <Button
+                  type="button"
+                  size="lg"
+                  variant="cta"
+                  onClick={() => setQrIsVisible(!qrIsVisible)}
+                >
+                  {qrIsVisible ? "Hide QR Code" : "Display QR Code"}
+                </Button>
+                {qrIsVisible && (
+                  <div className="qr-code">
+                    <QRCode bgColor="#eceff2" fgColor="" value={roomUrl} size={200} />
+                  </div>
+                )}
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
       <ToastContainer />
     </section>
