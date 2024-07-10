3df780c77d9b97e6aa8091ccbf5c362312fe5f37
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-10 17:26:50 +0200

    update input component style and layout according to new palette

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-burning-orange-300 bg-summer-green-100 px-3 py-2 text-sm text-summer-green-400 ring-offset-summer-green-100 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-summer-green-500/30 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-burning-orange-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+          "flex h-10 w-full rounded-md border border-pumpkin-400 bg-shark-100 px-3 py-2 text-sm text-gallery-800 ring-offset-gallery-100 file:border-0 file:bg-transparent file:text-sm placeholder:text-gallery-300/90 placeholder:font-light placeholder:italic focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-burning-orange-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
           className,
         )}
         ref={ref}
