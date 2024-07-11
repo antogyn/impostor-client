046d224b1c516864a5b4901c8d79aaa67a880efb
Noëllie Chatain Six <noellie.chatain@hotmail.com>
2024-07-11 14:46:52 +0200

    create LocaleSelector component

---
+++ src/components/App/LocaleSelector/LocaleSelector.tsx
@@ -0,0 +1,34 @@
+import i18n from "../../../i18n";
+import { Button } from "@/components/ui/button";
+
+export const LocaleSelector = () => {
+  const setLocale = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const activeLocale = localStorage.getItem("i18nextLng");
+  console.log(activeLocale);
+
+  return (
+    <div className="absolute top-4 right-4 flex items-center space-x-2">
+      <Button
+        className={`fr-btn ${activeLocale === "fr" ? "active-locale-btn" : ""}`}
+        type="button"
+        size="icon"
+        variant="outline"
+        onClick={() => setLocale("fr")}
+      >
+        🇫🇷
+      </Button>
+      <Button
+        className={`en-btn ${activeLocale === "en" ? "active-locale-btn" : ""}`}
+        type="button"
+        size="icon"
+        variant="outline"
+        onClick={() => setLocale("en")}
+      >
+        🇬🇧
+      </Button>
+    </div>
+  );
+};
