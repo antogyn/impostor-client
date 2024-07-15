a2eca5eaa7df9cf720157f2809cb12eb68b68b4d
Anthony Giniers <antogyn@gmail.com>
2024-07-15 13:55:40 +0200

    feat: add infinite ws reconnection attempts

--- src/graphql-client.ts
+++ src/graphql-client.ts
@@ -1,26 +1,33 @@
-import { Client, fetchExchange, subscriptionExchange } from 'urql';
-import { createClient as createWSClient } from 'graphql-ws';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { Client, fetchExchange, subscriptionExchange } from "urql";
+import { createClient as createWSClient } from "graphql-ws";
+import { cacheExchange } from "@urql/exchange-graphcache";
 
 const wsClient = createWSClient({
-  url: 'wss://impostor-server.fly.dev/graphql'
+  url: "wss://impostor-server.fly.dev/graphql",
+  shouldRetry: () => true,
+  retryAttempts: Number.POSITIVE_INFINITY,
+  retryWait: async (totalAttempts) => {
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.max(0.1 * (2 ** totalAttempts - 1), 2))
+    );
+  },
 });
 
 export const graphqlClient = new Client({
-  url: 'https://impostor-server.fly.dev/graphql',
+  url: "https://impostor-server.fly.dev/graphql",
   exchanges: [
     cacheExchange({}),
     fetchExchange,
     subscriptionExchange({
       forwardSubscription(request) {
-        const input = { ...request, query: request.query || '' };
+        const input = { ...request, query: request.query || "" };
         return {
           subscribe(sink) {
             const unsubscribe = wsClient.subscribe(input, sink);
             return { unsubscribe };
-          }
+          },
         };
-      }
-    })
-  ]
+      },
+    }),
+  ],
 });
