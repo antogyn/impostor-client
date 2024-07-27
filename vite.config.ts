909c5dba41c64bda285762a0a85d64ed49c46684
Anthony Giniers <antogyn@gmail.com>
2024-07-27 17:56:13 +0200

    feat: add minimal pwa configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'node:path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "node:path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.ico", "impostor.svg"],
+      manifest: {
+        name: "Impostor Online Game",
+        short_name: "Impostor",
+        description: "Impostor Online Game",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "impostor.svg",
+            sizes: "48x48 72x72 96x96 128x128 192x192 256x256 512x512",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
